329e02a6c2f02113297710245e39f687e27e0740
karichalobato <00002517@uca.edu.sv>
2019-06-02 21:45:25 -0600

    Creando GameDao

--- app/src/main/java/com/lobato/pdm_parcial01_00002517/database/daos/GameDAO.kt
+++ app/src/main/java/com/lobato/pdm_parcial01_00002517/database/daos/GameDAO.kt
@@ -1,4 +1,21 @@
 package com.lobato.pdm_parcial01_00002517.database.daos
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.lobato.pdm_parcial01_00002517.database.entities.Game
+
+
+@Dao
 interface GameDAO {
+    @Query("SELECT *FROM Game")
+    fun getAllGames():LiveData<List<Game>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGame(game: Game)
+
+    @Update
+    fun updateGame(game: Game)
+
+    @Query("DELETE FROM Game")
+    fun deleteGames()
 }
