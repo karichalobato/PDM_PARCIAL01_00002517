a5671db1dcaf4548695ae3c62679f5a6ab50b2ce
karichalobato <00002517@uca.edu.sv>
2019-06-02 22:16:12 -0600

    GameViewModel

--- app/src/main/java/com/lobato/pdm_parcial01_00002517/viewModel/GameViewModel.kt
+++ app/src/main/java/com/lobato/pdm_parcial01_00002517/viewModel/GameViewModel.kt
@@ -1,4 +1,27 @@
 package com.lobato.pdm_parcial01_00002517.viewModel
 
-class GameViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.lobato.pdm_parcial01_00002517.database.GameRoomDataBase
+import com.lobato.pdm_parcial01_00002517.database.entities.Game
+import com.lobato.pdm_parcial01_00002517.repository.GameRepository
+import kotlinx.coroutines.launch
+
+class GameViewModel (application: Application):AndroidViewModel(application) {
+
+    private  val gameRepository:GameRepository
+    val allGames:LiveData<List<Game>>
+
+    init {
+        val gameDao = GameRoomDataBase.getDatabase(application, viewModelScope)
+            .gameDAO()
+        gameRepository = GameRepository(gameDao)
+        allGames = gameRepository.allGame
+    }
+
+    fun insert(game: Game) = viewModelScope.launch {
+        gameRepository.insert(game)
+    }
 }
