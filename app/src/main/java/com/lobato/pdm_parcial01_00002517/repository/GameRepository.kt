2e7f2041940dce6ea1b7bf363133c8f9ebf8ab7d
karichalobato <00002517@uca.edu.sv>
2019-06-02 22:07:19 -0600

    GameRepository

--- app/src/main/java/com/lobato/pdm_parcial01_00002517/repository/GameRepository.kt
+++ app/src/main/java/com/lobato/pdm_parcial01_00002517/repository/GameRepository.kt
@@ -1,4 +1,18 @@
 package com.lobato.pdm_parcial01_00002517.repository
 
-class GameRepository {
+import androidx.lifecycle.LiveData
+import com.lobato.pdm_parcial01_00002517.database.daos.GameDAO
+import com.lobato.pdm_parcial01_00002517.database.entities.Game
+
+class GameRepository ( private val gameDAO:GameDAO){
+
+    val allGame: LiveData<List<Game>> = gameDAO.getAllGames()
+
+    suspend fun insert(game: Game){
+        gameDAO.insertGame(game)
+    }
+
+    suspend fun update(game: Game){
+        gameDAO.updateGame(game)
+    }
 }
